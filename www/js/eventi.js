bfc548874097a9e62abb06df40e3ff78b483cc52
Lorenzo Lombardi <bluemix@afbnet.it>
2018-11-07 16:48:43 +0100

    config.xml changes notifications introduced css fixes pull to refresh for every news page

--- www/js/eventi.js
+++ www/js/eventi.js
@@ -1,18 +1,20 @@
 function getEventi(e,page){
   Framework7.request.json(endPointUrl+'/Eventi/GetEventi', {}, function (data) {
     var listitems="";
-    // var url = '/eventidetail/eventiid/';
+    var url = '/eventidetail/eventiid/';
     for(i=0;i<data.length;i++){
-      listitems += newsBadge(data[i], '');
+      listitems += newsBadge(data[i], url+i);
     }
-    $$('#eventi-loader').remove();
+    if($$('#eventi-loader')){
+      $$('#eventi-loader').hide();
+    }
     $$('#eventi-container').html(listitems);
   });
 }
-// function getEventiDetail(e,page,nid){
-//   Framework7.request.json('http://serviceapp.ance.it:26031/ServiceAppAnce.svc/Eventi/GetEventi', {}, function (data) {
-//     newsItem = newsDetail(data,nid);
-//     $$('#eventidetail-loader').remove();
-//     $$('#eventidetail-container').html(newsItem);
-//   });
-// }
+function getEventiDetail(e,page,nid){
+  Framework7.request.json(endPointUrl+'/Eventi/GetEventi', {}, function (data) {
+    newsItem = newsDetail(data,nid);
+    $$('#eventidetail-loader').remove();
+    $$('#eventidetail-container').html(newsItem);
+  });
+}
